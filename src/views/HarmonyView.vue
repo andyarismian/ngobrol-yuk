6dacf931539a1c68fa7ccc47a5133eebec32e41a
Andy Arismianto <andy@kabayan.id>
2026-05-13 20:15:42 +0700

    add notif harmony

---
+++ src/views/HarmonyView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div id="main">
+        <div class="card">
+            <div class="card-body border-pixel bg-neutral-100">
+                Belum tersedia nih, tunggu update terbaru ya!
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+#main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+}
+</style>
